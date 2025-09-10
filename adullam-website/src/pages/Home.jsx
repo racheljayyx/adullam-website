f3d92a7c0b1fad193abf4ac155cdd00cd83b569c
Rachel Jaisey <jaiseyrachel@gmail.com>
2025-09-10 11:37:24 +0100

    feat(home): update Home page to include Core Values and Core Pillars components

--- adullam-website/src/pages/Home.jsx
+++ adullam-website/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Programs from '../components/Programs'
+import CoreValues from '../components/CoreValues'
+import CorePillars from '../components/CorePillars'
 
 function Home() {
     return (
         <div>
             <Hero />
             <About />
+            <CoreValues />
+            <CorePillars />
             <Programs />
         </div>
     )
